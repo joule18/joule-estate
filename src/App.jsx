da401225e73161466d06b65602a7903c00e9df0c
joule18 <jrjundam18@gmail.com>
2024-07-20 10:54:35 +0800

    created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+
 function App() {
-  return <div className="text-red-700">App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
 }
 export default App;
