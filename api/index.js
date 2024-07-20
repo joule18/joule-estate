0f4bd6c7bd92e359219c476e1d48b85f3d7d0237
joule18 <jrjundam18@gmail.com>
2024-07-20 12:42:06 +0800

    setup mongodb connection

--- api/index.js
+++ api/index.js
@@ -1,7 +1,25 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log(`Server is running on port 3000...`);
-});
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB");
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}...`);
+    });
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  }
+};
+
+start();
