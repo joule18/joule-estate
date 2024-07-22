1b9b9a5837d4f687ec0d3d51e9aa0fad017f5fff
joule18 <jrjundam18@gmail.com>
2024-07-22 14:10:46 +0800

    updated header and private profile page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import Profile from "./pages/Profile";
 
 //components
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -20,7 +21,9 @@ function App() {
         <Route path="/log-in" element={<Login />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </Router>
   );
