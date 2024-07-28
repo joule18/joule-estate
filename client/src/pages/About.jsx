12a916f3ee3a337d13c7da2726db4470e33ebd40
joule18 <jrjundam18@gmail.com>
2024-07-28 19:46:30 +0800

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,4 +1,13 @@
 function About() {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Joule Estate
+      </h1>
+      <p className="mb-4 text-slate-700"></p>
+      <p className="mb-4 text-slate-700 "></p>
+      <p className="mb-4 text-slate-700 "></p>
+    </div>
+  );
 }
 export default About;
