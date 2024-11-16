7d710d83dbaa55bdb3c6aee7d4c1a50e6f5e32ff
joule18 <jrjundam18@gmail.com>
2024-11-16 09:47:51 +0800

    commit3

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 const app = express();
 
@@ -14,12 +15,20 @@ import listingRouter from "./routes/listingRoute.js";
 
 ///////////////////////////////////////
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
