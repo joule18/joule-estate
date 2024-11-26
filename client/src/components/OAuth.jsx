c0f02799b6be5a89698beda9387ae814f1a1b556
joule18 <jrjundam18@gmail.com>
2024-11-26 17:43:27 +0800

    removed google login

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -37,14 +37,13 @@ function OAuth() {
       );
     }
   };
-  return (
-    <button
-      type="button"
-      onClick={handleGoogleClick}
-      className="bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95"
-    >
-      Continue with Google
-    </button>
-  );
+  return null;
+  // <button
+  //   type="button"
+  //   onClick={handleGoogleClick}
+  //   className="bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95"
+  // >
+  //   Continue with Google
+  // </button>
 }
 export default OAuth;
